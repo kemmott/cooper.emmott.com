691bc853fa9c75f21cb414bffbb8d3d295e2323f
root <root@emmott.net>
2016-04-04 04:32:09 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<body>
+<h1>Cooper!</h1>
+<p><img src="https://farm3.staticflickr.com/2028/1582467399_a2ca70a114.jpg" /></p>
+<p><script language="JavaScript" type="text/javascript">
+<!--
+	var first = 'ma';
+	var second = 'il';
+	var third = 'to:';
+	var address = '&#99;&#111;&#111;&#112;&#101;&#114;';
+	var domain = '&#101;&#109;&#109;&#111;&#116;&#116;&#46;&#99;&#111;&#109;'
+	document.write('<a href="');
+	document.write(first+second+third);
+	document.write(address);
+	document.write('&#64;');
+	document.write(domain);
+	document.write('" title="email Cooper">');
+	document.write('email Cooper<\/a>');
+// -->
+</script> to get the link to his pictures</p>
+
+</body>
+</html>
